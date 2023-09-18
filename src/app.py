7a142e05867ae5678d677857b74546337abae7ad
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-09-18 18:05:48 -0300

    :ok_hand: style: removing comments

--- src/app.py
+++ src/app.py
@@ -3,21 +3,16 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-# Define a route that listens for POST requests
 @app.route("/api/flask/splunk-example", methods=["POST"])
 def process_post_request():
-    # Get the raw text data from the request body
     raw_text = request.data.decode("utf-8")
 
-    # Split the raw text into lines (assuming each line represents a separate data entry)
     lines = raw_text.split("\n")
 
-    # Create an empty list to store the JSON objects
     json_list = []
 
     for i, line in enumerate(lines):
-        # Create a JSON object from the line (adjust as needed)
-        json_obj = {
+        json_mock_data_obj = {
             "preview": True,
             "offset": i,
             "result": {
@@ -36,13 +31,11 @@ def process_post_request():
             },
         }
 
-        # Add a "lastrow" key to the last JSON object in the list
         if i == len(lines) - 1:
-            json_obj["lastrow"] = True
+            json_mock_data_obj["lastrow"] = True
 
-        json_list.append(json_obj)
+        json_list.append(json_mock_data_obj)
 
-    # Return the JSON list as the response
     return jsonify(json_list)
 
 
